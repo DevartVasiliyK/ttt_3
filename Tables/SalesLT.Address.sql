50dedc34b969b56e8c0e6b79d058dc7498bf5c49
ddd <ddd>
2018-06-01 08:17:37 +0300

    1

---
+++ Tables/SalesLT.Address.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [SalesLT].[Address] (
+  [AddressID] [int] IDENTITY,
+  [AddressLine1] [nvarchar](60) NOT NULL,
+  [AddressLine2] [nvarchar](60) NULL,
+  [City] [nvarchar](30) NOT NULL,
+  [StateProvince] [dbo].[Name] NOT NULL,
+  [CountryRegion] [dbo].[Name] NOT NULL,
+  [PostalCode] [nvarchar](15) NOT NULL,
+  [rowguid] [uniqueidentifier] NOT NULL CONSTRAINT [DF_Address_rowguid] DEFAULT (newid()),
+  [ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF_Address_ModifiedDate] DEFAULT (getdate()),
+  CONSTRAINT [PK_Address_AddressID] PRIMARY KEY CLUSTERED ([AddressID]),
+  CONSTRAINT [AK_Address_rowguid] UNIQUE ([rowguid])
+)
+GO
+
+CREATE INDEX [IX_Address_AddressLine1_AddressLine2_City_StateProvince_PostalCode_CountryRegion]
+  ON [SalesLT].[Address] ([AddressLine1], [AddressLine2], [City], [StateProvince], [PostalCode], [CountryRegion])
+GO
+
+CREATE INDEX [IX_Address_StateProvince]
+  ON [SalesLT].[Address] ([StateProvince])
+GO
